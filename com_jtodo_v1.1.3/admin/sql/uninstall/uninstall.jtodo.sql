f322e0ab3cb10e7d54ad1031453e3a88b8b22741
Hanjo Hingsen <hanjo@hingsen.de>
2013-10-11 14:10:53 +0200

    com_jToDo Version 1.1.3 (Fix)

--- com_jtodo_v1.1.3/admin/sql/uninstall/uninstall.jtodo.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__jtodo_projects`; 
-DROP TABLE IF EXISTS `#__jtodo_categories`; 
-DROP TABLE IF EXISTS `#__jtodo_todos`; 
-DROP TABLE IF EXISTS `#__jtodo_mappings`; 
-DROP TABLE IF EXISTS `#__jtodo_visits`; 
